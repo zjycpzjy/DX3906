9dae8dda7985b174fc659e0ca460d2c05d440822
jerry <1278777885@qq.com>
2025-02-22 17:09:09 +0800

    配置github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import {viteStaticCopy} from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,7 +13,15 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
+    viteStaticCopy({
+      targets: [
+        { src: 'public/404.html', dest: './' }, // 将 public/404.html 复制到构建目录的根路径
+      ],
+    }),
   ],
+  build: {
+    outDir: 'dist' // 构建输出目录
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
