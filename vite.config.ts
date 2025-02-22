9abb0cfc9b90cc4f3c8b7cd1cd0393fcc50921d7
jerry <1278777885@qq.com>
2025-02-22 16:36:40 +0800

    配置github pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/DX3906/',
+  base:'./',
   plugins: [
     vue(),
     vueJsx(),
